df7bcc63a27cdbc300cdc43520a18f31e38203f5
Meet Vora <meetrajivvora@gmail.com>
2022-09-25 14:41:40 -0700

    finished scraper and emailer

--- scraper.py
+++ scraper.py
@@ -5,17 +5,16 @@ import emailer
 from time import sleep
 
 URL = "https://www.apple.com/shop/refurbished/mac/macbook-air"
-MINUTES_TO_SLEEP = 0.05
+MINUTES_TO_SLEEP = 20
 
 def scrape_apple_refurb_webpage():
     """
     Scrape Apple refurb website to find M2 Macbook Air deals.
     """
-
+    counter = 0
     while(True):
         response = requests.get("https://www.apple.com/shop/refurbished/mac/2020-macbook-air")
         soup = BeautifulSoup(response.content, 'html.parser')
-        print(soup.prettify())
 
         all_devices = soup.find("div", class_="rf-refurb-category-grid-no-js").ul
         content = []
@@ -23,9 +22,13 @@ def scrape_apple_refurb_webpage():
             for device in all_devices.find_all("li"):
                 if "MacBook Air Apple M2" in device.h3.a.text:
                     content.append(device)
+            print("GOING TO EMAILER")
             emailer.send_email(content)
+            break
         else:
             # sleep for MINUTES_TO_SLEEP minutes if couldn't find M2 Macbook Air
+            print("SLEEPING", counter)
+            counter += 1
             sleep(60 * MINUTES_TO_SLEEP)
 
     # <div class="rf-refurb-category-grid-no-js">
